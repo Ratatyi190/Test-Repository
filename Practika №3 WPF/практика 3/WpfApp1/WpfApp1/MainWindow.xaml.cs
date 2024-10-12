1e3267c49b266418fcdb4201ee874e5521c33a96
Ratatyi190 <ratlove180@gmail.com>
2024-10-12 17:44:48 +0500

    Add files via upload

---
+++ Practika №3 WPF/практика 3/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Student> Students { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Students = new List<Student>();
+            StudentsDataGrid.ItemsSource = Students;
+
+        }
+        public class Student
+        {
+            public string NameStudent { get; set; }
+            public string AgeStudent { get; set; }
+            public string BallStudent { get; set; }
+        }
+
+        private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            string namestudent = NameStudentTextBox.Text;
+            string agestudent = AgeStudentTextBox.Text;
+            string ballstudent = BallStudentTextBox.Text;
+
+            Students.Add(new Student
+            {
+                NameStudent = namestudent,
+                AgeStudent = agestudent,
+                BallStudent = ballstudent,
+            });
+            NameStudentTextBox.Clear();
+            AgeStudentTextBox.Clear();
+            BallStudentTextBox.Clear();
+
+        }
+    }
+}
---
+++ Practika №3 WPF/практика 3/Практика 3/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практика_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Student> Students { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Students = new List<Student>();
+            StudentsDataGrid.ItemsSource = Students;
+        }
+
+
+
+        private void StudentDataGrid_Click(object sender, RoutedEventArgs e)
+        {
+            string studentName = StudentNameTextBox.Text;
+            string ageName = StudentAgeTextBox.Text;
+            string credniball = StudentCredniBallTextBox.Text;
+
+            Students.Add(new Student
+            {
+                StudentName = studentName,
+                AgeName = ageName,
+                CredniBall = credniball,
+            });
+
+            StudentNameTextBox.Clear();
+            StudentAgeTextBox.Clear();
+            StudentCredniBallTextBox.Clear();
+
+        }
+    }
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public string AgeName { get; set; }
+        public string CredniBall { get; set; }
+    }
+}
+
+
+
